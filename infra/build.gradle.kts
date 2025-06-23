3d98d5847af831c2e54ba1d706875dddd82119a8
2hyunjinn <huenjin04@naver.com>
2025-06-23 20:46:25 +0900

    [feat] #6 infra 모듈 build.gradle.kts 파일 생성

---
+++ infra/build.gradle.kts
@@ -0,0 +1,24 @@
+plugins {
+    kotlin("jvm")
+    kotlin("plugin.spring")
+    id("org.springframework.boot")
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    //aws
+    implementation("org.springframework.cloud:spring-cloud-starter-aws:2.2.6.RELEASE")
+
+    // Spring context (Bean 등록용)
+    implementation("org.springframework.boot:spring-boot-starter")
+
+    // Kotlin 필수 모듈
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+
+    // 테스트
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
