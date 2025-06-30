a72bbf450dd135dc167b8d24dc648d6aa15ce133
2hyunjinn <huenjin04@naver.com>
2025-06-30 21:38:19 +0900

    [fix] #8 리소스 생성 시에는 HTTP 201을 반환하도록 수정

--- api/src/main/kotlin/gongbaek/api/common/ResponseBuilder.kt
+++ api/src/main/kotlin/gongbaek/api/common/ResponseBuilder.kt
@@ -7,7 +7,7 @@ object ResponseBuilder {
         ResponseEntity.ok(ApiResponse.success(ResponseSuccess.OK, data))
 
     fun <T> created(data: T): ResponseEntity<ApiResponse<T>> =
-        ResponseEntity.ok(ApiResponse.success(ResponseSuccess.CREATED, data))
+        ResponseEntity.status(201).body(ApiResponse.success(ResponseSuccess.CREATED, data))
 
     fun error(responseError: ResponseError): ResponseEntity<ApiResponse<Void>> =
         ResponseEntity.status(responseError.httpStatus)
