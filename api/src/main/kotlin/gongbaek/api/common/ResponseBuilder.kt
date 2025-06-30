3c6833940dc7af57e554657d9e5188243bd8a3b0
2hyunjinn <huenjin04@naver.com>
2025-06-30 21:14:33 +0900

    [feat] #7 공통 API Response 구조 설계

---
+++ api/src/main/kotlin/gongbaek/api/common/ResponseBuilder.kt
@@ -0,0 +1,15 @@
+package gongbaek.api.common
+
+import org.springframework.http.ResponseEntity
+
+object ResponseBuilder {
+    fun <T> ok(data: T): ResponseEntity<ApiResponse<T>> =
+        ResponseEntity.ok(ApiResponse.success(ResponseSuccess.OK, data))
+
+    fun <T> created(data: T): ResponseEntity<ApiResponse<T>> =
+        ResponseEntity.ok(ApiResponse.success(ResponseSuccess.CREATED, data))
+
+    fun error(responseError: ResponseError): ResponseEntity<ApiResponse<Void>> =
+        ResponseEntity.status(responseError.httpStatus)
+            .body(ApiResponse.error(responseError))
+}
